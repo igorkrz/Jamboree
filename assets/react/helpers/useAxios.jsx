b4356ca4904c3497d2b7e0586b8813eec48d8282
igorkrz <krznarigoryt@gmail.com>
2024-10-31 16:12:09 +0100

    Minor fix

--- assets/react/helpers/useAxios.jsx
+++ assets/react/helpers/useAxios.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 const useAxios = axios.create({
     baseURL: process.env.NODE_ENV === 'production'
         ? 'http://49.13.134.200:8380'
-        : 'http://localhost:8380'
+        : 'http://localhost:8380',
     headers: {
         'Accept': 'application/json+ld'
     }
